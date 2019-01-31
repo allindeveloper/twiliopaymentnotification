d380e834069f377a258e472a5fb080f7d358c185
Uchendu Precious <uchendubozz@gmail.com>
2019-01-31 14:41:04 +0100

    Minor Update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -28,33 +28,7 @@ class Welcome extends CI_Controller {
 		$data['name'] = "Uchendu Precious";
 		$data['phone'] = "+2347034367931";
 		$this->load->view('purchase_view', $data);
-        // $data = ['phone' => '+2347034367931', 'text' => 'Sending SMS wit Twill for PayStack Integration Just Worked'];
-		// print_r($this->sendSMS($data));
-		
-
     }
  
-    protected function sendSMS($data) {
-          // Your Account SID and Auth Token from twilio.com/console
-            $sid = 'AC37a5a07f2c41d65e5f4e600d6d189aa9';
-            $token = '0c803cf87f8961d682710870a323c090';
-            $client = new Client($sid, $token);
-            
-            // Use the client to do fun stuff like send text messages!
-            $sent =   $client->messages->create(
-                // the number you'd like to send the message to
-                $data['phone'],
-                array(
-                    // A Twilio phone number you purchased at twilio.com/console
-                    "from" => "+19897182290",
-                    // the body of the text message you'd like to send
-                    'body' => $data['text']
-                )
-			);
-			if($sent){
-				return 'Message Sent';
-			}else{
-				return 'Message Not Sent';
-			}
-    }
+
 }
